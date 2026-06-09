a0e35ac96a654b73a20f74e586d5124636f325e2
IIHTDevelopers <ayan.mal201@gmail.com>
2026-06-09 16:22:30 +0530

    First commit

--- Milestone 4/Milestone_4/src/test/java/Pages/Openkart_POM.java
+++ Milestone 4/Milestone_4/src/test/java/Pages/Openkart_POM.java
@@ -1,5 +1,26 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Openkart_POM {
+	WebDriver driver;
+	WebDriverWait wait;
+	JavascriptExecutor js;
+	
+	//Registration
+	By Account=By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/i");
+	By Registration=By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a");
+	By First_name=By.id("input-firstname");
+	By Last_name=By.id("input-lastname");
+	
+	public Openkart_POM(WebDriver driver) {
+		this.driver=driver;
+	}
+	public void Registration() {
+		
+	}
 
 }
