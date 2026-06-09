f19242db5ccf736a6c6a7142aba8414262307374
IIHTDevelopers <ayan.mal201@gmail.com>
2026-06-09 15:36:18 +0530

    First commit

---
+++ Milestone 4/Milestone_4/src/test/java/Openkart_demo/Openkart_test.java
@@ -0,0 +1,19 @@
+package Openkart_demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Openkart_test {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
