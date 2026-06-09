5dff0f80781e881c7f60608cb60bc41094aa259d
IIHTDevelopers <ayan.mal201@gmail.com>
2026-06-09 16:32:32 +0530

    First commit

--- Milestone 4/Milestone_4/src/test/java/Pages/Openkart_POM.java
+++ Milestone 4/Milestone_4/src/test/java/Pages/Openkart_POM.java
@@ -15,11 +15,41 @@ public class Openkart_POM {
 	By Registration=By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a");
 	By First_name=By.id("input-firstname");
 	By Last_name=By.id("input-lastname");
-	
+	By Email=By.id("input-email");
+	By Telephone=By.id("input-telephone");
+	By Password=By.id("input-password");
+	By cnf_password=By.id("input-confirm");
+	By Checkbox=By.xpath("//*[@id=\"content\"]/form/div/div/input[1]");
+	By submit=By.xpath("//*[@id=\"content\"]/form/div/div/input[2]");
 	public Openkart_POM(WebDriver driver) {
 		this.driver=driver;
 	}
-	public void Registration() {
+	public void Registration() throws InterruptedException {
+		driver.findElement(Account).click();
+		  driver.findElement(Registration).click();
+		  Thread.sleep(2000);
+
+			driver.findElement(First_name).sendKeys("Ayan");
+			
+			driver.findElement(Last_name).sendKeys("Mal");
+
+			
+			driver.findElement(Email).sendKeys("Ayan1234@gmail.com");
+			
+			driver.findElement(Telephone).sendKeys("1234567890");
+
+			
+			driver.findElement(Password).sendKeys("Ayan@123");
+			
+			driver.findElement(cnf_password).sendKeys("Ayan@123");
+			Thread.sleep(2000);
+			driver.findElement(Checkbox).click();
+
+			Thread.sleep(2000);
+			driver.findElement(submit).click();
+
+			Thread.sleep(3000);
+
 		
 	}
 
