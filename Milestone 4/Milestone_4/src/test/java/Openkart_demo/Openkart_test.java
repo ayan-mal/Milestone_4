71947624f0a19baae2893651e4c1642bbe6c3b6c
IIHTDevelopers <ayan.mal201@gmail.com>
2026-06-09 15:39:43 +0530

    First commit

--- Milestone 4/Milestone_4/src/test/java/Openkart_demo/Openkart_test.java
+++ Milestone 4/Milestone_4/src/test/java/Openkart_demo/Openkart_test.java
@@ -2,15 +2,24 @@ package Openkart_demo;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class Openkart_test {
+	WebDriver driver;
+	WebDriverWait wait;
+	@BeforeTest(alwaysRun=true)
+	  public void beforeTest() {
+		driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		}
   @Test
   public void f() {
   }
-  @BeforeTest
-  public void beforeTest() {
-  }
+  
 
   @AfterTest
   public void afterTest() {
